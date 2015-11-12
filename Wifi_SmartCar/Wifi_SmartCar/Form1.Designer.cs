65196f20da0420987e4d9945abda372ec2825e7d
kaylorchen <kaylor.chen@qq.com>
2015-11-12 16:49:49 +0800

    C#
    wifi smart car

---
+++ Wifi_SmartCar/Wifi_SmartCar/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace Wifi_SmartCar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.videoSourcePlayer = new AForge.Controls.VideoSourcePlayer();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "192.168.1.139";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(453, 18);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(35, 21);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "8080";
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(758, 183);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 51);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "前进";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            this.button1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button1_MouseUp);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(671, 239);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(81, 54);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "左转";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
+            this.button2.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button2_MouseUp);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(842, 239);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(76, 55);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "右转";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button3_MouseDown);
+            this.button3.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button3_MouseUp);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(758, 300);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(78, 54);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "后退";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button4_MouseDown);
+            this.button4.MouseUp += new System.Windows.Forms.MouseEventHandler(this.button4_MouseUp);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(676, 433);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(246, 110);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "连接";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "IP 地址";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(394, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "视频端口";
+            // 
+            // videoSourcePlayer
+            // 
+            this.videoSourcePlayer.Location = new System.Drawing.Point(12, 63);
+            this.videoSourcePlayer.Name = "videoSourcePlayer";
+            this.videoSourcePlayer.Size = new System.Drawing.Size(640, 480);
+            this.videoSourcePlayer.TabIndex = 10;
+            this.videoSourcePlayer.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer.VideoSource = null;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(610, 18);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(33, 21);
+            this.textBox4.TabIndex = 11;
+            this.textBox4.Text = "2001";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(551, 21);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "控制端口";
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(758, 239);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(79, 55);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "鸣笛";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(955, 556);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.videoSourcePlayer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "SmartCar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void Button1_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ Wifi_SmartCar/Wifi_SmartCar/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using AForge.Video;
+using AForge.Video.DirectShow;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Wifi_SmartCar
+{
+    public partial class Form1 : Form
+    {
+        private string url;
+        private IPAddress ip;
+        private Socket clientSocket=new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        public Form1()
+        {
+            InitializeComponent();
+         }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(button5.Text=="连接")
+            {
+                button5.Text = "断开连接";
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                textBox4.Enabled = false;
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button4.Enabled = true;
+                button3.Enabled = true;
+                button6.Enabled = true;
+                ip = IPAddress.Parse(textBox1.Text);
+                try
+                {
+                    clientSocket.Connect(ip,int.Parse(textBox4.Text));
+                }
+                catch { }
+                url = "http://" + textBox1.Text + ":"+textBox2.Text + "/";
+                MJPEGStream mjpegSource = new MJPEGStream(url + "?action=stream");
+                OpenVideoSource(mjpegSource);
+            }
+            else
+            {
+                button5.Text = "连接";
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                textBox4.Enabled = true;
+                videoSourcePlayer.Stop();
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button4.Enabled = false;
+                button3.Enabled = false;
+                button6.Enabled = false;
+            }
+            
+
+
+        }
+        private void OpenVideoSource(IVideoSource source)
+        {
+            this.Cursor = Cursors.WaitCursor;
+            videoSourcePlayer.SignalToStop();
+            videoSourcePlayer.WaitForStop();
+            videoSourcePlayer.VideoSource = source;
+            videoSourcePlayer.Start();
+            this.Cursor = Cursors.Default;
+        }
+
+        private void button1_MouseDown(object sender, EventArgs e)
+        {
+            try
+            {
+                string sendMessage = "lsya" ;
+                clientSocket.Send(Encoding.ASCII.GetBytes(sendMessage));
+
+            }
+            catch
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        }
+        private void button1_MouseUp(object sender, EventArgs e)
+        {
+            try
+            {
+                string sendMessage = "lsys";
+                clientSocket.Send(Encoding.ASCII.GetBytes(sendMessage));
+
+            }
+            catch
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        }
+        private void button2_MouseDown(object sender, EventArgs e)
+        {
+            try
+            {
+                string sendMessage = "lsyb";
+                clientSocket.Send(Encoding.ASCII.GetBytes(sendMessage));
+
+            }
+            catch
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        }
+        private void button2_MouseUp(object sender, EventArgs e)
+        {
+            try
+            {
+                string sendMessage = "lsys";
+                clientSocket.Send(Encoding.ASCII.GetBytes(sendMessage));
+
+            }
+            catch
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        }
+
+        private void button3_MouseDown(object sender, EventArgs e)
+        {
+            try
+            {
+                string sendMessage = "lsyc";
+                clientSocket.Send(Encoding.ASCII.GetBytes(sendMessage));
+
+            }
+            catch
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        }
+        private void button3_MouseUp(object sender, EventArgs e)
+        {
+            try
+            {
+                string sendMessage = "lsys";
+                clientSocket.Send(Encoding.ASCII.GetBytes(sendMessage));
+
+            }
+            catch
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        }
+
+        private void button4_MouseDown(object sender, EventArgs e)
+        {
+            try
+            {
+                string sendMessage = "lsyd";
+                clientSocket.Send(Encoding.ASCII.GetBytes(sendMessage));
+
+            }
+            catch
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        }
+        private void button4_MouseUp(object sender, EventArgs e)
+        {
+            try
+            {
+                string sendMessage = "lsys";
+                clientSocket.Send(Encoding.ASCII.GetBytes(sendMessage));
+
+            }
+            catch
+            {
+                clientSocket.Shutdown(SocketShutdown.Both);
+                clientSocket.Close();
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (button6.Text == "鸣笛")
+            {
+                try
+                {
+                    string sendMessage = "lsyz";
+                    clientSocket.Send(Encoding.ASCII.GetBytes(sendMessage));
+
+                }
+                catch
+                {
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+                button6.Text = "关闭鸣笛";
+            }
+            else
+            {
+                try
+                {
+                    string sendMessage = "lsyy";
+                    clientSocket.Send(Encoding.ASCII.GetBytes(sendMessage));
+
+                }
+                catch
+                {
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+                button6.Text = "鸣笛";
+            }
+
+        }
+    }
+
+}
